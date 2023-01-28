89e852a7ad280a1a9c328fb3b9de1db439ecc2ec
rany <ranygh@riseup.net>
2023-01-28 18:46:51 +0200

    Add subtitles support and increase efficiency

--- code/text2speech.py
+++ code/text2speech.py
@@ -20,11 +20,12 @@ class Text2Speech:
       
       home_path = "output"
 
-      filename = title + " - " + author + ".mp3"
-      filename = filename.replace(",", "")
-      filename = filename.replace("?", "")
-      filename = filename.replace("!", "")
-      filename = filename.replace("*", "")
+      mp3_filename = title + " - " + author + ".mp3"
+      mp3_filename = mp3_filename.replace(",", "")
+      mp3_filename = mp3_filename.replace("?", "")
+      mp3_filename = mp3_filename.replace("!", "")
+      mp3_filename = mp3_filename.replace("*", "")
+      webvtt_filename = mp3_filename[:-len(".mp3")] + ".vtt"
 
       say_title = True
 
@@ -35,16 +36,15 @@ class Text2Speech:
          if os.path.isfile(os.path.join(home_path, s))]
       lst_files.sort(key=lambda s: os.path.getctime(os.path.join(home_path, s)))
 
+      file_tk = ''
       for input_file in lst_files:
-         print("Converting", input_file)
          file_path = os.path.join(home_path, input_file)
-         
          if os.path.isfile(file_path) and re.search(r'\.txt$', file_path):
+            print("Converting", input_file)
             with open(file_path, 'r', encoding='utf-8') as f:
                lines = f.read()
-               file_tk = ''
                if say_title:
-                  file_tk = title + ' de ' + author + "\n"
+                  file_tk += title + ' de ' + author + "\n"
                   say_title = False
                   
                for item in range(len(data_dict["chars"])):
@@ -53,7 +53,19 @@ class Text2Speech:
                for tk in sent_tokenize(lines):
                   file_tk += tk + '\n'
 
-               #https://docs.microsoft.com/en-US/azure/cognitive-services/speech-service/language-support
-               communicate = edge_tts.Communicate(file_tk, input_voice)
-               await communicate.save(os.path.join('output', filename))
+               # Add extra newline to signal new paragraph to TTS
+               file_tk += '\n'
+
+      #https://docs.microsoft.com/en-US/azure/cognitive-services/speech-service/language-support
+      communicate = edge_tts.Communicate(file_tk, input_voice)
+      submaker = edge_tts.SubMaker()
+      with open(os.path.join('output', mp3_filename), "wb") as file:
+         async for chunk in communicate.stream():
+            if chunk["type"] == "audio":
+               file.write(chunk["data"])
+            elif chunk["type"] == "WordBoundary":
+               submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"])
+
+      with open(os.path.join('output', webvtt_filename), "w", encoding='utf-8') as file:
+         file.write(submaker.generate_subs())
       print("End")
